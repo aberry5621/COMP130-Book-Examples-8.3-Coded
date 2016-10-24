a9a5ed9675d9798ff379939b31c1422c8282f79c
aberry5621 <aberry5621@gmail.com>
2016-10-24 08:45:15 -0700

    Initial Commit

---
+++ COMP130 Book Examples 8.3 Coded/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  COMP130 Book Examples 8.3 Coded
+//
+//  Created by ax on 10/24/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
